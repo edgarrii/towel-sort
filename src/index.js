aef5172275849382c061012eaa95151dacd2b64d
edgarrii <71753826+edgarrii@users.noreply.github.com>
2021-02-28 15:33:45 +0300

    feat:  change function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    for (let i = 0; i < matrix.length; i++){
+    if (matrix[i] == 0) {
+      return matrix[i];
+    } else {
+        let mass = [];
+        mass = matrix[0];
+        for (let i = 1; i < matrix.length; i++) {
+      if (i % 2 == 0) {
+         mass = mass.concat(matrix[i]);
+    } else mass = mass.concat(matrix[i].reverse());
+    }
+    return mass;
+    }
+  }
 }
