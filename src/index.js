7a280c3345ca5b74923c55bc00b484c8ba0c7e83
edgarrii <71753826+edgarrii@users.noreply.github.com>
2021-02-28 16:10:25 +0300

    fix: fix bug with test

--- src/index.js
+++ src/index.js
@@ -2,20 +2,17 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-      if (matrix.isArray()) {
-    for (let i = 0; i < matrix.length; i++){
-        if (matrix[i] == 0) {
-          return matrix[i];
-        } else {
-            let mass = [];
-            mass = matrix[0];
-            for (let i = 1; i < matrix.length; i++) {
-          if (i % 2 == 0) {
-            mass = mass.concat(matrix[i]);
+     if (Array.isArray(matrix) == true) {
+    if (matrix.length == 0) {
+      return [];
+    }
+      let mass = [];
+      mass = matrix[0];
+      for (let i = 1; i < matrix.length; i++) {
+        if (i % 2 == 0) {
+           mass = mass.concat(matrix[i]);
         } else mass = mass.concat(matrix[i].reverse());
-        }
-        return mass;
-        }
-  }
+      }
+    return mass;
   }
 }
